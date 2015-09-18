d98b898f530f8a2cc8d63e379ecae080ed5d08f9
Jemd13 <jemd311@gmail.com>
2015-09-18 16:21:38 -0430

    Comentarios,pruebas y main

--- Etapa1/LexBot.py
+++ Etapa1/LexBot.py
@@ -16,17 +16,17 @@
 import sys
 import ply.lex as lex
 
-print(sys.argv[1])
-f = open(sys.argv[1],'r') # Abre el archivo escrito en el terminal para
-                               # futura lectura
-finput = f.read()
 
-# Nombres de los tokens
+f = open(sys.argv[1],'r') # Abre el archivo pasado como parametro por linea de comando
+finput = f.read()
+  
+# Palabras reservadas
 reservadas = {
-  'create'        : 'TkCreate',
   'int'           : 'TkInt',
   'bool'          : 'TkBool',
   'char'          : 'TkChar',
+
+  'create'        : 'TkCreate',
   'bot'           : 'TkBot',
   'on'            : 'TkOn',
   'activation'    : 'TkActivation',
@@ -52,6 +52,7 @@ reservadas = {
   'false'         : 'TkFalse',
 }
 
+# Nombres de los demas tokens
 tokens = [
    'TkIdent',     
    'TkNum',          
@@ -78,7 +79,6 @@ tokens = [
 ] + list(reservadas.values())
 
 # Reglas de expresiones regulares simples
-
 t_TkComa         = r'\,'
 t_TkPunto        = r'\.'
 t_TkDosPuntos    = r'\:'
@@ -98,56 +98,72 @@ t_TkMayor        = r'\>'
 t_TkMayorIgual   = r'\>\=' # ***
 t_TkIgual        = r'\='
 
+# Funcion para detectar comentarios
 def t_TkComentario(t):
-    r'((\$\-([^\-]|(\-)+[^\$])*\-\$)|(\$\$.*))'
-    t.lexer.lineno += len(t.value.rsplit('\n')) - 1
+  r'((\$\-([^\-]|(\-)+[^\$])*\-\$)|(\$\$.*))'
+  t.lexer.lineno += len(t.value.rsplit('\n')) - 1
 
+# Funcion para deteccion de caracteres
 def t_TkCaracter(t):
-    r'\'.*\''
-    return t
+  r'\'.*\''
+  return t
 
+# Funcion para deteccion de identificadores de variables
 def t_TkIdent(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reservadas.get(t.value,'TkIdent')
-    return t
+  r'[a-zA-Z_][a-zA-Z_0-9]*'
+  t.type = reservadas.get(t.value,'TkIdent')
+  return t
 
+# Funcion para deteccion de numeros
 def t_TkNum(t):
-    r'\d+'
-    t.value = int(t.value)    
-    return t
+  r'\d+'
+  t.value = int(t.value)    
+  return t
 
+# Funcion para deteccion de saltos de linea
 def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+  r'\n+'
+  t.lexer.lineno += len(t.value)
 
-# Compute column. 
-#     input is the input text string
-#     token is a token instance
+# Funcion para calcular el numero de columna de cada token
 def NumColumna(input,token):
-    last_cr = input.rfind('\n',0,token.lexpos)
-    # if last_cr < 0:
-    #   last_cr = -1
-    column = (token.lexpos - last_cr)
-    return column
+  last_cr = input.rfind('\n',0,token.lexpos)
+  column = (token.lexpos - last_cr)
+  return column
 
+# Funcion para deteccion de caracteres ilegales
 def t_error(t):
-    print("Error: Caracter inesperado \"%s\" en la fila %d, columna %d " % (t.value[0], t.lineno, NumColumna(finput, t))) 
-    t.lexer.skip(1)
+  print("Error: Caracter inesperado \"%s\" en la fila %d, columna %d " % (t.value[0], t.lineno, NumColumna(finput, t))) 
+  t.lexer.skip(1)
 
 # String que ignora caracteres como los espacios y los tab
 t_ignore  = ' \t'
 
-lexer = lex.lex()
+# Funcion principal
+def main() :
 
-lexer.input(finput)
+  lexer = lex.lex()
+  lexer.input(finput)
 
-# Este ciclo tokeniza en el lexer
-for tok in lexer:
+  lista = []
+  # for tok in lexer:
+  #   lista.append(tok)
+
+  # for x in lista:
+  #   print(x)
+
+
+  # Este ciclo tokeniza en el lexer 
+  for tok in lexer:
     if (tok.type != 'TkIdent') and (tok.type != 'TkCaracter') and (tok.type != 'TkNum'):
-        print(tok.type, tok.value, tok.lineno, NumColumna(finput, tok))
-    elif (tok.type == 'TkIdent') and (tok.type != 'TkCaracter'):
-        print(tok.type+"(\""+tok.value+"\")", tok.lineno, NumColumna(finput, tok))
-    elif (tok.type != 'TkIdent') and ((tok.type == 'TkCaracter') or (tok.type == 'TkNum')):
-        print(tok.type+"("+str(tok.value)+")", tok.lineno, NumColumna(finput, tok))
-    
-f.close()
+      print(tok.type, tok.value, tok.lineno, NumColumna(finput, tok))
+    elif (tok.type == 'TkIdent') :
+      print(tok.type+"(\""+tok.value+"\")", tok.lineno, NumColumna(finput, tok))
+    elif (tok.type == 'TkCaracter') or (tok.type == 'TkNum'):
+      print(tok.type+"("+str(tok.value)+")", tok.lineno, NumColumna(finput, tok))
+      
+  f.close()
+
+# Programa principal
+if __name__ == "__main__":
+  main()
