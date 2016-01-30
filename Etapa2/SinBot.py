71ab449258fc0b227b80f30603ef863555f61ca1
Jemd13 <jemd311@gmail.com>
2016-01-30 17:33:44 -0430

    Yay como que funciona :D

--- Etapa2/SinBot.py
+++ Etapa2/SinBot.py
@@ -18,42 +18,43 @@ import ply.lex as lex
 import ply.yacc as yacc
 
 from LexBot import BotLexer
+tokens = BotLexer.tokens
 
 def p_expression_t_TkSuma(p):
-    'expression : expression t_TkSuma term'
-    p[0] = p[1] + p[3]
+  'expression : expression TkSuma term'
+  p[0] = p[1] + p[3]
 
 def p_expression_minus(p):
-    'expression : expression t_TkResta term'
-    p[0] = p[1] - p[3]
+  'expression : expression TkResta term'
+  p[0] = p[1] - p[3]
 
 def p_expression_term(p):
-    'expression : term'
-    p[0] = p[1]
+  'expression : term'
+  p[0] = p[1]
 
 def p_term_times(p):
-    'term : term t_TkMult factor'
-    p[0] = p[1] * p[3]
+  'term : term TkMult factor'
+  p[0] = p[1] * p[3]
 
 def p_term_div(p):
-    'term : term t_TkDiv factor'
-    p[0] = p[1] / p[3]
+  'term : term TkDiv factor'
+  p[0] = p[1] / p[3]
 
 def p_term_factor(p):
-    'term : factor'
-    p[0] = p[1]
+  'term : factor'
+  p[0] = p[1]
 
 def p_factor_num(p):
-    'factor : TkInt'
-    p[0] = p[1]
+  'factor : TkNum'
+  p[0] = p[1]
 
 def p_factor_expr(p):
-    'factor : t_TkParAbre expression t_TkParCierra'
-    p[0] = p[2]
+  'factor : TkParAbre expression TkParCierra'
+  p[0] = p[2]
 
 # Error rule for syntax errors
 def p_error(p):
-    print("Syntax error in input!")
+  print("Syntax error in input!")
 
 # f = open(sys.argv[1],'r') # Abre el archivo pasado como parámetro por línea 
 #                             # de comando
@@ -65,11 +66,18 @@ botlex.build()
 # Build the parser
 parser = yacc.yacc()
 
-while True:
-   try:
-       s = raw_input('bot > ')
-   except EOFError:
-       break
-   if not s: continue
-   result = parser.parse(lexer=botlex.lexer)
-   print(result)
+# try:
+#     s = raw_input('bot > ')
+# except EOFError:
+#     break
+# if not s: continue
+
+f = open(sys.argv[1],'r') # Abre el archivo pasado como parámetro por línea 
+                        # de comando
+finput = f.read()
+
+result = parser.parse(finput, lexer=botlex.lexer)
+
+print(result)
+
+f.close()
