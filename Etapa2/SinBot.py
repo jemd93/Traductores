4a3e773de2b418941a6e6f9747145b9d5eaa3da3
Jemd13 <jemd311@gmail.com>
2016-01-30 15:11:11 -0430

    Primer commit de etapa 2 :D

---
+++ Etapa2/SinBot.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+
+#!/usr/bin/env python3  
+
+# -------------------------------------------------
+#  Universidad Simón Bolívar
+#  Traductores e interpretadores - CI3725
+#  Prof. Ricardo Monascal
+#
+#  Autores: Jorge Marcano   # Carnet 11-10566
+#           Meggie Sánchez  # Carnet 11-10939
+#
+# Proyecto BOT - Etapa 2 - Análisis Sintactico
+# -------------------------------------------------
+
+import sys
+import ply.lex as lex
+import ply.yacc as yacc
+
+from LexBot import tokens 
+
